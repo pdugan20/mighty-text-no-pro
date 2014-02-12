22bbd8bdc7e8bab737e9f8e9586ac65c8f89d4bf
Patrick Dugan <dugan.pat@gmail.com>
2014-02-12 00:12:00 -0800

    initial commit
    chrome extension that modifies display property for button elements in
    MightyText web app user interface

---
+++ no-pro.js
@@ -0,0 +1,4 @@
+document.getElementById("goProButton").style.display="none";
+document.getElementById("alert-bottom-right-corner").style.display="none";
+document.getElementById("share-button-main").style.display="none";
+document.getElementById("follow-button-main").style.display="none";
