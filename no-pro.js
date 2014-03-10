dcb5b90852e1e535e499a82efcbe3b40718f2cf1
Patrick Dugan <dugan.pat@gmail.com>
2014-03-09 23:23:43 -0700

    deleted extra js
    removed redundant javascript

--- no-pro.js
+++ no-pro.js
@@ -1,4 +1,7 @@
 document.getElementById("goProButton").style.display="none";
-document.getElementById("alert-bottom-right-corner").style.display="none";
 document.getElementById("share-button-main").style.display="none";
 document.getElementById("follow-button-main").style.display="none";
+
+window.onload = function() {
+  document.getElementById("alert-bottom-right-corner").style.display="none";
+}
